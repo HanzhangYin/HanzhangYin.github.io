faabbb50e9603d07dea312d48cff88570c7f57e7
HanzhangYin <y2315657163@gmail.com>
2023-06-10 22:51:33 -0400

    update

---
+++ tutoring/highlight.js
@@ -0,0 +1,17 @@
+window.addEventListener('hashchange', function() {
+    // Remove the highlight from all sections
+    let sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.classList.remove('highlight');
+    });
+
+    // Get the new hash
+    let hash = window.location.hash;
+
+    // Add the highlight to the new section
+    let section = document.querySelector(hash);
+    if (section) {
+        section.classList.add('highlight');
+    }
+});
+
