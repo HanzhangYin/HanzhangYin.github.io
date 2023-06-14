6c739f262bc0cff30d27aa623ea17d29c7649d5c
HanzhangYin <y2315657163@gmail.com>
2023-06-14 14:21:30 -0400

    update

--- experiment/lick.js
+++ experiment/lick.js
@@ -1,12 +1,10 @@
-<script>
-  // Select all the links in the navbar
-  var navLinks = document.querySelectorAll('.navbar a');
+// Select all the links in the navbar
+var navLinks = document.querySelectorAll('.navbar a');
 
-  // For each link, add a click event
-  navLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
-      // When a link is clicked, uncheck the checkbox
-      document.getElementById('nav-toggle').checked = false;
-    });
+// For each link, add a click event
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    // When a link is clicked, uncheck the checkbox
+    document.getElementById('nav-toggle').checked = false;
   });
-</script>
+});
