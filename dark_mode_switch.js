2058422b89c8342b5e9582d92c125186236e78c1
HanzhangYin <y2315657163@gmail.com>
2023-07-11 13:25:32 -0400

    update

--- dark_mode_switch.js
+++ dark_mode_switch.js
@@ -1,6 +1,10 @@
 // Assuming you have a switch on your page to toggle dark mode on or off
-let darkModeSwitch = document.getElementById('dark-mode-switch');
+// let darkModeSwitch = document.getElementById('dark-mode-switch');
 
-darkModeSwitch.addEventListener('change', function() {
-    document.body.classList.toggle('dark-mode', this.checked);
+// darkModeSwitch.addEventListener('change', function() {
+//     document.body.classList.toggle('dark-mode', this.checked);
+// });
+
+document.getElementById('dark-mode-toggle').addEventListener('change', function(event) {
+    document.body.classList.toggle('dark-mode', event.target.checked);
 });
