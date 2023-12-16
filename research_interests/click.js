62b702598447bd94fd714747a9a47a035f61e631
HanzhangYin <y2315657163@gmail.com>
2023-12-16 08:46:52 -0600

    update

---
+++ research_interests/click.js
@@ -0,0 +1,10 @@
+// Select all the links in the navbar
+var navLinks = document.querySelectorAll('.navbar a');
+
+// For each link, add a click event
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    // When a link is clicked, uncheck the checkbox
+    document.getElementById('nav-toggle').checked = false;
+  });
+});
