0f12b305f2b4e01bb924138755d345c653185691
HanzhangYin <y2315657163@gmail.com>
2023-12-31 11:14:51 -0500

    Refactor copyCode function and update code container styling

--- elseworlds/copy.js
+++ elseworlds/copy.js
@@ -1,8 +1,11 @@
-function copyCode() {
-    var code = document.getElementById("code").innerText;
+function copyCode(button) {
+    // Find the <pre> element that is a sibling of the clicked button
+    var code = button.previousElementSibling.innerText;
+
     navigator.clipboard.writeText(code).then(() => {
         alert("Code copied to clipboard!");
     }).catch(err => {
         console.error('Error in copying text: ', err);
     });
 }
+
