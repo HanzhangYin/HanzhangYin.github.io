a6db53464babc7c82c26dd53c2c59bbe8813a5ca
HanzhangYin <y2315657163@gmail.com>
2024-08-24 13:14:07 -0500

    edit the new file

---
+++ note/algebra/mousemove.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener('mousemove', function (event) {
+    const arrow = document.querySelector('.next-page-arrow');
+    const rightEdgeThreshold = 50; // distance from right edge to trigger the arrow
+
+    if (window.innerWidth - event.clientX < rightEdgeThreshold) {
+        arrow.style.right = '10px';
+        arrow.style.opacity = '1';
+    } else {
+        arrow.style.right = '-50px';
+        arrow.style.opacity = '0';
+    }
+});
