db9bd04680ec3be9371c605fecb9b4a88ab7cf5a
HanzhangYin <y2315657163@gmail.com>
2024-08-24 13:43:04 -0500

    update

--- note/algebra/mousemove.js
+++ note/algebra/mousemove.js
@@ -1,7 +1,7 @@
 // script.js
 
 document.addEventListener('mousemove', function (event) {
-    const arrow = document.querySelector('.next-page-arrow');
+    const arrow = document.querySelector('.next-page-bar');
     const rightEdgeThreshold = 100; // distance from right edge to trigger the arrow
 
     if (window.innerWidth - event.clientX < rightEdgeThreshold) {
