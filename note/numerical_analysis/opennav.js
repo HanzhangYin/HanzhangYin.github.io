3ac12f91af3c43007c79460b058e89f81d40a3af
HanzhangYin <y2315657163@gmail.com>
2024-08-27 09:26:10 -0500

    add new fiels

---
+++ note/numerical_analysis/opennav.js
@@ -0,0 +1,28 @@
+// Initially hide the navigation bar when the website is first loaded on a phone screen
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.innerWidth <= 768) {
+        closeNav();
+    }
+});
+
+function openNav() {
+    if (window.innerWidth > 768) {
+        document.getElementById("mySidenav").style.width = "250px";
+    } else {
+        document.getElementById("mySidenav").style.width = "100%";
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Condition: When a link is clicked, the script checks if the screen width is 768px or less (indicating a phone screen).
+// If so, it triggers the closeNav() function to close the sidebar.
+document.querySelectorAll('#mySidenav a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        if (window.innerWidth <= 768) { // Only close on smaller screens
+            closeNav();
+        }
+    });
+});
