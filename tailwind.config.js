83759c54582585c4fa11187934bf3ea2e2f4c016
HanzhangYin <y2315657163@gmail.com>
2025-02-23 09:28:01 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./**/*.html", "./styles.css"],
+  content: [
+    "./**/*.{html,js}", // scan all HTML and JS files in your project
+  ],
+  safelist: [
+    'p-4', // add any classes you use with @apply that might not be found in your content
+    // add others if needed (e.g., 'p-8', 'm-4', etc.)
+  ],
   theme: {
     extend: {},
   },
