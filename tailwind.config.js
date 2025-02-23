4e1b26b3c4015dd39a5fb8d0a1e1cbce552f6962
HanzhangYin <y2315657163@gmail.com>
2025-02-23 08:35:23 -0600

    add: implement applicant information section and Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './**/*.html', // This will include all HTML files in all subdirectories
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
